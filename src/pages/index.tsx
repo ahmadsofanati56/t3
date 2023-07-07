fd398a2c75c8cf3e83876f1730f71b9c11957300
ahmadsofanati56 <ahmadsofanati56@gmail.com>
2023-07-08 01:17:49 +0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 import { api } from "y/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const {data} = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -18,6 +18,9 @@ export default function Home() {
       {!!user.isSignedIn && <SignOutButton />} 
 
       <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <div>
+        {data?.map((post)=> (<div key={post.id}>{post.content}</div>))}
+      </div>
       </main>
     </>
   );
