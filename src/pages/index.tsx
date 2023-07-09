80316b84f25348c6731fb0d2ab833c1a780f6055
ahmadsofanati56 <ahmadsofanati56@gmail.com>
2023-07-09 14:36:17 +0300

    debugging2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,9 +56,9 @@ export default function Home() {
 
       {user.isSignedIn && <CreatePostWizard/>} 
       </div>
-      {/* <div className="flex flex-col">
-        {...data!?.map((fullPost)=> (<PostView {...fullPost} key={fullPost.post.id}/>))}
-      </div> */}
+      <div className="flex flex-col">
+        {[...data!]?.map((fullPost)=> (<PostView {...fullPost} key={fullPost.post.id}/>))}
+      </div>
       </div>
       </main>
     </>
